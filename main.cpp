5fb86ea3657abbfd43eddf77e7cafb03611a475a
fpeter10 <90526558+fpeter10@users.noreply.github.com>
2023-11-08 13:00:55 +0100

    Add files via upload
    Updated main

--- main.cpp
+++ main.cpp
@@ -11,6 +11,12 @@ int main()
     std::cout<< "Use sqrt(number) to calculate square root of a number"<< std::endl;
     std::cout<< "Use pow(num1 ,num2) to calculate num2 power of a num1"<< std::endl;
     std::cout<< "Use fact(number) to calculate factorial of a number"<< std::endl;
+    std::cout<< "Use prime(number) to calculate n th prime number"<< std::endl;
+    std::cout<< "Use sinus(number) to calculate sinus of an angle"<< std::endl;
+    std::cout<< "Use cosinus(number) to calculate cosinus of an angle"<< std::endl;
+    std::cout<< "Use tangent(number) to calculate tangent of an angle"<< std::endl;
+    std::cout<< "Use nthroot(n, m) to calculate m th root of n"<< std::endl;
+    std::cout<< "Use combination(n, m) to calculate under n the k"<< std::endl;    
     std::cout<< "Use # to define a variable like # x= 1;"<< std::endl;
     std::cout<< "Type in exit to exit"<< std::endl;
 	
